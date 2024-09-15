af82eaeee68bce902b55e36788508808611a1382
grizeus <zaytsewww@gmail.com>
2024-09-15 22:53:41 +0300

    improve styling a bit

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -4,25 +4,27 @@ import TransactionItem from "./TransactionItem";
 
 export default function TransactionHistory({ transactions }) {
   return (
-    <table className={css.transactionHistory}>
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {transactions.map(transaction => (
-          <TransactionItem
-            key={transaction.id}
-            type={transaction.type}
-            amount={transaction.amount}
-            currency={transaction.currency}
-          />
-        ))}
-      </tbody>
-    </table>
+    <div className={css.tableContainer}>
+      <table className={css.transactionHistory}>
+        <thead>
+          <tr>
+            <th>Type</th>
+            <th>Amount</th>
+            <th>Currency</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map(transaction => (
+            <TransactionItem
+              key={transaction.id}
+              type={transaction.type}
+              amount={transaction.amount}
+              currency={transaction.currency}
+            />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
